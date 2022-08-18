0c9eea18b30126b2fa67d4e744c4f2d6c69895f5
TechX <61621188+smit1298@users.noreply.github.com>
2022-08-19 00:07:46 +0100

    modified books prices alongside responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,18 @@ module.exports = {
     ],
     theme: {
       extend: {
-        screens: {
-          "2xl": { max: "2560px" },
-          // => @media (max-width: 1535px) { ... }
-          xl: { max: "1279px" },
-          // => @media (max-width: 1279px) { ... }
-          lg: { max: "1025px" },
-          // => @media (max-width: 1023px) { ... }
-          md: { max: "768px" },
-          // => @media (max-width: 767px) { ... }
-          sm: { max: "639px" },
-          // => @media (max-width: 639px) { ... }
-        },
+        // screens: {
+        //   "2xl": { max: "2560px" },
+        //   // => @media (max-width: 1535px) { ... }
+        //   xl: { max: "1279px" },
+        //   // => @media (max-width: 1279px) { ... }
+        //   lg: { max: "1025px" },
+        //   // => @media (max-width: 1023px) { ... }
+        //   md: { max: "768px" },
+        //   // => @media (max-width: 767px) { ... }
+        //   sm: { max: "639px" },
+        //   // => @media (max-width: 639px) { ... }
+        // },
       },
     },
     plugins: [],
