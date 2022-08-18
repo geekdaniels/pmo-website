f52dd6e130b81e944a5b4c4d8c3b4c3bbdd7b9b9
TechX <61621188+smit1298@users.noreply.github.com>
2022-08-19 00:17:49 +0100

    removed comments from books top and reduce font of available books in store on small screen

--- components/books-list/BooksTop.jsx
+++ components/books-list/BooksTop.jsx
@@ -6,17 +6,13 @@ import amabooks from "../../public/images/amazon_Books.png";
 export default function BooksTop() {
   return (
     <div className="relative bg-black py-5">
-      {/* <div className="absolute left-[90px] bg-white w-2 h-[110px] lg:left-10"></div> */}
       <div className="absolute left-10 bg-white w-2 h-[110px] sm:left-[90px]"></div>
-      {/* <h2 className="ml-[120px] text-xl bg-[#42cbf5] w-fit py-3 items-center rounded font-semibold px-2 lg:mx-[90px]"> */}
       <h2 className=" text-xl bg-[#42cbf5] w-fit py-3 items-center rounded font-semibold px-2 mx-[90px] sm:ml-[120px]">
         Read and unveil the GREATNESS in you
       </h2>
-      {/* <h1 className="ml-[120px] my-5 text-white font-bold text-4xl lg:ml-[90px]"> */}
       <h1 className="ml-[90px] my-5 text-white font-bold text-4xl sm:ml-[120px]">
         BOOKS BY PMO
       </h1>
-      {/* <div className="flex justify-between bg-[#42cbf5] my-5 rounded py-5 pl-[30px] ml-[90px] w-[85%] lg:mx-auto lg:block lg:w-[85%]"> */}
       <div className="block mx-auto w-[85%] bg-[#42cbf5] my-5 rounded py-5 pl-[30px] sm:ml-[90px] sm:flex sm:justify-between">
         <div className="flex mr-8">
           <Image
@@ -26,8 +22,8 @@ export default function BooksTop() {
             alt="amazon books"
           />
         </div>
-        <div className="">
-          <h3 className="mr-[150px] text-5xl font-semibold sm:flex sm:justify-end">
+        <div>
+          <h3 className="mr-[150px] mt-3 text-xl  font-semibold sm:mt-0 sm:text-5xl sm:flex sm:justify-end">
             See available books <br /> Visit the store
           </h3>
           <button
@@ -38,7 +34,6 @@ export default function BooksTop() {
           </button>
         </div>
       </div>
-      {/* <div className="lg:block flex"> */}
       <div className="block sm:flex">
         {data.map((book) => (
           <Books key={book.id} book={book} />
