23109d850737555076df236a84aa656273008c01
Sakinbarnes Samuel <samuel.sakinbarnes@sevenbridgesolutions.com>
2022-08-20 10:24:55 +0100

    Commit merge changes

--- books_data.jsx
+++ books_data.jsx
@@ -1,17 +1,17 @@
 const data = [
   {
-    id: 1,
+    id: "1",
     img: "/images/intimate.png",
     title: "Get intimate",
-    price: "2500",
+    price: "$100",
     amazon: "Get on amazon",
     icon: "/icons/amazon.svg",
   },
   {
-    id: 2,
+    id: "2",
     img: "/images/purpose_alignment.png",
     title: "Purpose Alignment",
-    price: "2500",
+    price: "$100",
     amazon: "Get on amazon",
     icon: "/icons/amazon.svg",
   },
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      screens: {
-        "2xl": { max: "2560px" },
-        // => @media (max-width: 1535px) { ... }
-        xl: { max: "1279px" },
-        // => @media (max-width: 1279px) { ... }
-        lg: { max: "1025px" },
-        // => @media (max-width: 1023px) { ... }
-        md: { max: "768px" },
-        // => @media (max-width: 767px) { ... }
-        sm: { max: "639px" },
-        // => @media (max-width: 639px) { ... }
-      },
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+  }
