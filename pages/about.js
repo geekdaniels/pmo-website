52407f21228cd19f53c42b4f018738270ba85479
Sakinbarnes Samuel <samuel.sakinbarnes@sevenbridgesolutions.com>
2022-08-20 11:57:54 +0100

    Changed page name

--- pages/about.js
+++ pages/about.js
@@ -3,8 +3,8 @@ import AboutBody from "../components/about/AboutBody";
 import AboutFooter from "../components/about/AboutFooter";
 const aboutPage = () => {
     return (
-        <div className="relative bg-sky-500 h-full">
-            <div className="px-12 m-12  bg-sky-300 text-justify bg-gradient-to-r from-cyan-400 to-blue-300">
+        <div className="relative  h-full">
+            <div className="p-12 m-12 bg-transparent text-justify bg-gradient-to-r from-cyan-400 to-blue-300">
                 <AboutHeader />
                 <AboutBody />
                 <AboutFooter />
