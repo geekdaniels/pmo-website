9dd7f90139ed37557053ee49bd42135865b21651
geekdaniels <oladotun.olufemi@yahoo.com>
2022-08-24 00:54:12 +0100

    clean up landing page

--- components/layout/navbar.jsx
+++ components/layout/navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="mx-auto px-4 w-[90%] bg-white border-gray-200 px-2 sm:px-4 py-2.5">
+      <nav className="px-4 bg-white border-gray-200 px-2 sm:px-4 py-2.5">
         <div className="container flex flex-wrap md:flex-nowrap justify-between items-center mx-auto">
           <Link href="/" className="flex items-center">
             <img
@@ -44,14 +44,14 @@ const Navbar = () => {
             <ul className="flex flex-col md:flex-row ml-auto p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
               <li>
                 <Link href="/" aria-current="page">
-                  <a className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white">
+                  <a className="block py-2 pr-4 pl-3 text-white rounded md:bg-transparent md:text-blue-700 md:p-0">
                     Home
                   </a>
                 </Link>
               </li>
               <li>
                 <Link href="/about">
-                  <a className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                  <a className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">
                     About
                   </a>
                 </Link>
@@ -59,7 +59,7 @@ const Navbar = () => {
 
               <li>
                 <Link href="/bookings">
-                  <a className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                  <a className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">
                     Bookings
                   </a>
                 </Link>
@@ -67,7 +67,7 @@ const Navbar = () => {
 
               <li>
                 <Link href="/contact">
-                  <a className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                  <a className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">
                     Contact
                   </a>
                 </Link>
