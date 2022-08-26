61f2c97ded5fec152716e128a283008ad9d89b2c
Sakinbarnes Samuel <samuel.sakinbarnes@sevenbridgesolutions.com>
2022-08-26 03:30:46 +0100

    About page design

--- components/about/AboutBody.jsx
+++ components/about/AboutBody.jsx
@@ -1,14 +1,83 @@
+import Image from "next/image";
+import pmo1 from "../../public/images/pmo1.JPG"
+import pmo2 from "../../public/images/pmo2.JPG"
+import pmo3 from "../../public/images/pmo3.JPG"
+import pmo4 from "../../public/images/pmo4.JPG"
+import pmo5 from "../../public/images/pmo5.JPG"
 const AboutBody = () => {
   return (
-    <div className="flex flex-col gap-8 text-xl m-4 text-justify" >
-      <div><span className="font-semibold text-2xl">Lorem ipsum dolor sit amet consectetur</span>, adipisicing elit. Error nam harum hic veniam enim explicabo iusto. Accusantium facilis aperiam accusamus libero tenetur, quibusdam, expedita ipsam qui assumenda maxime placeat magnam officiis eligendi temporibus fugiat debitis possimus ipsum et autem? Modi eius, beatae obcaecati nihil perspiciatis eligendi placeat omnis quas ratione at quis doloremque iusto veniam est culpa hic ipsum! Inventore fugit nam recusandae maxime in! <span className="font-semibold text-2xl">Mollitia assumenda aperiam nulla delectus.</span></div>
-      <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque aut iure ea, <span className="font-semibold text-2xl">pariatur ducimus dolorum cupiditate alias obcaecati et placeat exercitationem sequi libero quidem saepe corrupti suscipit earum molestiae reprehenderit eos deleniti </span>cum quam velit aperiam quia! Consequatur labore numquam, perspiciatis ducimus non exercitationem rerum autem laudantium consequuntur voluptates magni?</div>
-      <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis eum labore officia placeat?<span className="font-semibold text-2xl"> Autem eos, delectus</span> ,ad cumque mollitia doloremque similique saepe cupiditate, laudantium commodi doloribus voluptatum quibusdam ab aperiam.</div>
-      <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa praesentium omnis quaerat, <span className="font-semibold text-2xl">itaque illo ipsam repellat corrupti</span> facere est, iusto fugiat. Soluta cupiditate deserunt tenetur in, quibusdam sit labore ab?</div>
-      <div>Lorem ipsum dolor<span className="font-semibold text-2xl">sit amet consectetur adipisicing elit</span>. Cupiditate delectus quidem, aut omnis neque soluta explicabo iusto placeat, enim magnam fugiat sequi ipsum magni, quas labore voluptates consequuntur? Saepe amet eum hic debitis eveniet voluptates inventore consequuntur tempora. Dolorum ratione excepturi quod porro provident quidem repellat? Iste aut eligendi voluptatum.</div>
-      <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit maiores, fuga voluptas ea nobis nostrum deserunt vel.<span className="font-semibold text-2xl"> Culpa, quis ratione!</span></div>
-      <div>Lorem ipsum dolor sit amet <span className="font-semibold text-2xl">consectetur adipisicing elit. Neque perferendis reprehenderit quos, </span>atque laudantium doloremque iure fugit obcaecati aliquid ipsa!</div>
+    <div>
+      <div className="bg-slate-200 lg:pb-16 md:pb-4">
+        <div className="lg:flex md:container md:m-autow-full w-full max-h-fit pb-5">
+          <div className="font-sans font-normal text-lg md: text-start m-auto p-8">
+            <h4><span className="font-semibold text-2xl">Lorem ipsum dolor sit amet consectetur</span> adipisicing elit. Molestiae, nam sapiente culpa doloremque amet delectus eveniet, nihil saepe magnam at illum iste commodi in blanditiis, consectetur quia velit repellat cumque laborum autem? <span className="font-semibold text-2xl">Dignissimos nobis quasi consequuntur</span> excepturi aut temporibus. Non obcaecati culpa hic veritatis similique quas esse laborum eligendi velit?</h4>
+          </div>
+          <div className="" width={2400} height={3600}>
+            <Image src={pmo2}
+              className="object-cover object-top h-full lg:scale-90" alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-sky-100">
+        <div className="lg:flex md:container md:m-auto w-full">
+          <div className="md:container p-4">
+            <Image src={pmo3} className=" object-cover h-full " alt="" />
+          </div>
+          <div className="font-sans font-normal text-lg m-auto md:text-start p-8">
+            <h4><span className="font-semibold text-2xl">Lorem ipsum dolor sit amet consectetur</span> adipisicing elit. Molestiae, nam sapiente culpa doloremque amet delectus eveniet, nihil saepe magnam at illum iste commodi in blanditiis, consectetur quia velit repellat cumque laborum autem? <span className="font-semibold text-2xl">Dignissimos nobis quasi consequuntur</span> excepturi aut temporibus. Non obcaecati culpa hic veritatis similique quas esse laborum eligendi velit?</h4>
+          </div>
+
+        </div>
+      </div>
+
+      <div className="bg-slate-200  pb-8">
+        <div className="lg:flex md:container md:m-auto w-full">
+
+          <div className="font-sans font-normal text-lg md:text-justify m-auto lg:text-justify p-8">
+            <h4><span className="font-semibold text-2xl">Lorem ipsum dolor sit amet consectetur</span> adipisicing elit. Molestiae, nam sapiente culpa doloremque amet delectus eveniet, nihil saepe magnam at illum iste commodi in blanditiis, consectetur quia velit repellat cumque laborum autem? <span className="font-semibold text-2xl">Dignissimos nobis quasi consequuntur</span> excepturi aut temporibus. Non obcaecati culpa hic veritatis similique quas esse laborum eligendi velit?</h4>
+          </div>
+
+        </div>
+      </div>
+
+      <div className="bg-sky-50">
+        <div className="lg:flex md:container md:m-autow-full w-full max-h-fit pb-5">
+
+          <div className="font-sans font-normal text-lg m-auto p-8">
+            <h4><span className="font-semibold text-2xl">Lorem ipsum dolor sit amet consectetur</span> adipisicing elit. Molestiae, nam sapiente culpa doloremque amet delectus eveniet, nihil saepe magnam at illum iste commodi in blanditiis, consectetur quia velit repellat cumque laborum autem? <span className="font-semibold text-2xl">Dignissimos nobis quasi consequuntur</span> excepturi aut temporibus. Non obcaecati culpa hic veritatis similique quas esse laborum eligendi velit?</h4>
+          </div>
+          <div className="" width={2400} height={3600}>
+            <Image src={pmo4}
+              className="object-cover object-top h-full lg:scale-75 " alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-slate-300">
+        <div className="lg:flex md:container md:m-autow-full w-full max-h-fit pb-2">
+          <div className="md:container" width={2400} height={3600}>
+            <Image src={pmo5}
+              className="object-cover object-top h-full lg:scale-90 " alt="" />
+          </div>
+          <div className="font-sans font-normal text-justify text-lg m-auto p-8">
+            <h4><span className="font-semibold text-2xl">Lorem ipsum dolor sit amet consectetur</span> adipisicing elit. Molestiae, nam sapiente culpa doloremque amet delectus eveniet, nihil saepe magnam at illum iste commodi in blanditiis, consectetur quia velit repellat cumque laborum autem? <span className="font-semibold text-2xl">Dignissimos nobis quasi consequuntur</span> excepturi aut temporibus. Non obcaecati culpa hic veritatis similique quas esse laborum eligendi velit?</h4>
+          </div>
+
+        </div>
+      </div>
+
+      <div className="lg:flex flex-wrap lg:mx-auto lg:gap-8 md:container md:m-auto md:py-12 justify-center w-full px-8 bg-sky-100 py-4">
+        <div className="w-full  md:scale-75 pt-2 md:pt-5">
+          <Image src={pmo1} className="object-contain  w-auto h-auto mx-auto " alt="" />
+        </div>
+        <div className="w-full font-sans font-normal  text-lg text-center mx-auto ">
+          <h4><span className="font-semibold text-2xl">Lorem ipsum dolor sit amet consectetur</span> adipisicing elit. Molestiae, nam sapiente culpa doloremque amet delectus eveniet, nihil saepe magnam at illum iste commodi in blanditiis, consectetur quia velit repellat cumque laborum autem? <span className="font-semibold text-2xl">Dignissimos nobis quasi consequuntur</span> excepturi aut temporibus. Non obcaecati culpa hic veritatis similique quas esse laborum eligendi velit?</h4>
+        </div>
+
+      </div>
     </div>
+
   )
 }
 export default AboutBody
